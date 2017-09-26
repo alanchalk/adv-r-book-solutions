dc66026873260394be2760d162f1f6b1bb6fb3c6
alanchalk <ucabac0@live.ucl.ac.uk>
2017-09-26 09:10:17 +0100

    Update exercise4.r
    Peter, I am not sure that you answer above totally explains what is going on; see function g1 in the code, which according to your description should also return x.  I might be making a complete meal of this - but I admit is does puzzle me....

--- 11_nonstandard_eval/01_capturing/exercise4.r
+++ 11_nonstandard_eval/01_capturing/exercise4.r
@@ -14,3 +14,48 @@ g(x)
 # [1] "x"
 g(x + y ^ 2 / z + exp(a * sin(b)))
 # [1] "x"
+
+# Above explanation can be extended, since the answer given that `substitute(x)` is always "x"
+# is not so clear.  For example....
+# First, note:
+g1 <- function(x) deparse(substitute(x))
+
+# does work as expected
+g1(1:10)
+# [1] "1:10"
+
+# We can see a little more what is going on by adding a print statement to f
+f <- function(x) {
+    print(paste("function f returns: ", deparse(substitute(x))))
+    substitute(x)}
+g <- function(x) {
+    deparse(f(x))}
+
+f(1:10)
+# [1] "function f returns:  1:10"
+# 1:10
+# so when just calling f, returns 1:10
+# However when calling f from within g....
+g(1:10)
+# "function f returns:  x"
+# [1] "x"
+# by the time we call f, it is not able to return 1:10.
+
+# Just maybe to confuse things a bit more
+g2 <- function(x) {
+    substitute(x) %>% deparse()}
+g2(1:10)
+[1] "1:10"
+g2(x + y ^ 2 / z + exp(a * sin(b)))
+# [1] "x + y^2/z + exp(a * sin(b))"
+
+# I'm actually not sure what the asnwer is, I think it is to do with what the promise is within each function.
+# When just calling only f, the promise is made up of an expression which is 1:10 and an environment to evaluate it
+# The same is true when we use the chain iterator within g.  
+# However i think when we call f(x) from within g, then moving to a different environment is causing the different behaviour,
+# though I do not understand why.  I think maybe, the promise gets evaluated and looses the expression that used to go with it...?
+
+
+
+
+
